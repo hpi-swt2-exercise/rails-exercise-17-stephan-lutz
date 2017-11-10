6ad81c4e333a1db0db28947b49e9dccb16a78613
Stephan Lutz <stephan.lutz@student.hpi.de>
2017-11-10 20:02:21 +0100

    Created association between papers and authors. Closes #25.

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,4 +1,7 @@
 class Paper < ActiveRecord::Base
+
+  has_and_belongs_to_many :authors
+
   validates :title, presence: true,
                     length: { minimum: 1 }
   validates :venue, presence: true,
