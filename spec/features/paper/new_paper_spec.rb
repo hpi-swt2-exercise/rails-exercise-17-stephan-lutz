16a83c6d3ab2c03bae5801f097b7864435e29e98
Stephan Lutz <stephan.lutz@student.hpi.de>
2017-11-10 19:14:31 +0100

    Renames visible labels of a paper's title from 'Name' to 'Title'. Closes #20.

--- spec/features/paper/new_paper_spec.rb
+++ spec/features/paper/new_paper_spec.rb
@@ -6,4 +6,12 @@ describe "New paper page", type: :feature do
     visit new_paper_path
   end
 
+  it "should have text input for title, venue and year" do
+    visit new_author_path
+  
+    expect(page).to have_field('Title')
+    expect(page).to have_field('Venue')
+    expect(page).to have_field('Year')
+  end
+
 end
