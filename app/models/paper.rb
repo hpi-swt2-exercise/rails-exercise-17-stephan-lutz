7a356ddb8be02bcc0e973cf2388e611087c6f991
Stephan Lutz <stephan.lutz@student.hpi.de>
2017-11-10 19:43:35 +0100

    Adds validation to paper model, disallowing non-integer values. Closes #24.

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -3,5 +3,5 @@ class Paper < ActiveRecord::Base
                     length: { minimum: 1 }
   validates :venue, presence: true,
                     length: { minimum: 1 }
-  validates :year, presence: true
+  validates :year, presence: true, numericality: { only_integer: true }
 end
