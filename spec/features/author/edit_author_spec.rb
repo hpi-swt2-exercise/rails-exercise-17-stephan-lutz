b3e52bb5adf77ac7835e03a50c3041e846e0a57c
Stephan Lutz <stephan.lutz@student.hpi.de>
2017-11-10 10:49:47 +0100

    Adds failing test for #17.

--- spec/features/author/edit_author_spec.rb
+++ spec/features/author/edit_author_spec.rb
@@ -9,4 +9,13 @@ describe "Edit author page", type: :feature do
   it "should render withour error" do
     visit edit_author_path(@author)
   end
+
+  it "should save changes" do
+    visit edit_author_path(@author)
+
+    fill_in "author_first_name", :with => "Alan"
+    fill_in "author_last_name", :with => "Mathison"
+    fill_in "author_homepage", :with => "http://wikipedia.de/Alan_Turing"
+    find('input[type="submit"]').click
+  end
 end
