f1579b3dcbaf21f6c1a198e575bbe7b9f9d93b73
Stephan Lutz <stephan.lutz@student.hpi.de>
2017-11-10 20:34:38 +0100

    Adds failing test for #27.

--- spec/features/paper/new_paper_spec.rb
+++ spec/features/paper/new_paper_spec.rb
@@ -14,4 +14,13 @@ describe "New paper page", type: :feature do
     expect(page).to have_field('Year')
   end
 
+  it "should allow to select 5 authors from 5 seperate drop downs" do
+    visit new_paper_path
+
+    expect(page).to have_field('Author 1')
+    expect(page).to have_field('Author 2')
+    expect(page).to have_field('Author 3')
+    expect(page).to have_field('Author 4')
+    expect(page).to have_field('Author 5')
+  end
 end
