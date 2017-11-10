2277b32626351a44b5e37bae6a5e3dd5e0d21673
Stephan Lutz <stephan.lutz@student.hpi.de>
2017-11-10 19:25:54 +0100

    Fixes paper factory. Fixes test for #21. Adds validation for #21. Closes #21.

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
   it "should not validate without title" do
-    paper = create(:paper, :title => nil)
-    expect(paper).to not_be(valid)
+    paper = build(:paper, :title => nil)
+    expect(paper).not_to be_valid
   end
 end
