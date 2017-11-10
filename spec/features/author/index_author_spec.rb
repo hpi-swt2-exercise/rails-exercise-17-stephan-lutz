21603fcd61be650acdfca01e89f9771da9ee70bf
Stephan Lutz <stephan.lutz@student.hpi.de>
2017-11-10 09:16:27 +0100

    Fixes test for the link to the new author page.

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -13,7 +13,6 @@ describe "Author index page", type: :feature do
 
   it "should link to the new author page" do
     visit authors_path
-    expect(page).to have_css("a", text: "add author")
     click_link 'Add author'
     expect(page).to have_current_path(new_author_path)
   end
